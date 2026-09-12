71fc919b1dfe4890cafd95f976bb80cd32ddd49e
rlikhon <rlikhon@gmail.com>
2026-09-13 00:18:31 +0600

    frontend setup subscription

---
+++ frontend/src/apolloClient.js
@@ -0,0 +1,26 @@
+import { ApolloClient, InMemoryCache, split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
+
+const httpLink = new HttpLink({uri: 'http://localhost:4000/graphql'});
+const wsLink = new GraphQLWsLink(createClient({url: 'ws://localhost:4000/graphql'}));
+
+const splitLink = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink
+);
+
+const client = new ApolloClient({
+    link: splitLink,
+    cache: new InMemoryCache(),
+});
+
+export default client;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,11 +6,7 @@ import './index.css'
 import App from './App.jsx'
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 import { ApolloProvider } from '@apollo/client/react'
-
-const client = new ApolloClient({
-  link: new HttpLink({ uri: "http://localhost:4000/graphql" }),
-  cache: new InMemoryCache()
-});
+import client from './apolloClient';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
